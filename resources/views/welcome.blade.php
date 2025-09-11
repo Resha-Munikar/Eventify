52a9a9b3a2aef4409c9c720a4f40e4a530759733
Resha Munikar <resa.munikar@gmail.com>
2025-09-11 17:05:36 +0545

    Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"
       rel="stylesheet"
     />
+    
     <style>
       .brand-color {
         background-color: #8d85ec;
